87c90a093c314a003d253b9a49a556443cae6752
Harshil-1799 <shahharshil1706@gmail.com>
2023-08-23 18:30:46 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  default = "As Per Requirement"
+}
+variable "access_key" {
+  default = "As Per Requirement"
+}
+variable "secret_key" {
+  default = "As Per Requirement"
+}
